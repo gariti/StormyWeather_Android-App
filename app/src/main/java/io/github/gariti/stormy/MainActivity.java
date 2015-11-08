e329448be80299499fdafe9be267f692a879916d
Garrett Carver <gariti@gmail.com>
2015-11-07 16:00:31 -0800

    clean code, remove broken feature (to be re-enabled later)

--- app/src/main/java/io/github/gariti/stormy/MainActivity.java
+++ app/src/main/java/io/github/gariti/stormy/MainActivity.java
@@ -18,23 +18,21 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.FusedLocationProviderApi;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.model.LatLng;
+
 import com.squareup.okhttp.Call;
 import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.text.BreakIterator;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -43,11 +41,9 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     private static final String TAG = MainActivity.class.getSimpleName();
     private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
     private CurrentWeather mCurrentWeather;
-    private CurrentLocation mCurrentCity;
     private GoogleApiClient mGoogleApiClient;
     private double mLatitude;
     private double mLongitude;
-    private LocationRequest mLocationRequest;
 
     @Bind(R.id.timeLabel) TextView mTimeLabel;
     @Bind(R.id.degreeTextView) TextView mTemperatureLabel;
@@ -57,7 +53,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     @Bind(R.id.iconImageView) ImageView mIconImageView;
     @Bind(R.id.refreshImageView) ImageView mRefreshImageView;
     @Bind(R.id.progressBar) ProgressBar mProgressBar;
-    @Bind(R.id.locationLabel) TextView mLocationLabel;
+    //@Bind(R.id.locationLabel) TextView mLocationLabel; UPCOMING- update with City and State using reverse geocoding
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,31 +61,27 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         ButterKnife.bind(this);
 
         mProgressBar.setVisibility(View.INVISIBLE);
-
-        // Create the LocationRequest object
-        mLocationRequest = LocationRequest.create()
-                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-                .setInterval(10000)        // 10 seconds, in milliseconds
-                .setFastestInterval(1000); // 1 second, in milliseconds
-
         mRefreshImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getForecast(mLatitude, mLongitude);
-                getCityData(mLatitude, mLongitude);
+                if (mLatitude != 0 & mLongitude != 0) {
+                    Log.d(TAG, "Getting Forecast.....");
+                    getForecast(mLatitude, mLongitude);
+                }
             }
         });
-
-        mTemperatureLabel = (TextView)findViewById(R.id.degreeTextView);
-
-        getForecast(mLatitude, mLongitude);
-        Log.d(TAG, "Main UI code is running!");
 
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .addApi(LocationServices.API)
                 .build();
+
+        if (mLatitude != 0 & mLongitude != 0) {
+            Log.d(TAG, "Getting Forecast.....");
+            getForecast(mLatitude, mLongitude);
+        }
+        Log.d(TAG, "Main UI code is running!");
     }
     @Override
     protected void onResume() {
@@ -106,74 +98,17 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         }
     }
 
-    private void getCityData(double latitude, double longitude) {
-        String googleApiKey = getString(R.string.googleApiKey);
-        String googleLocationDataURL = "https://maps.googleapis.com/maps/api/geocode/json?latlng="+  latitude + "," + longitude + "&key=" + googleApiKey;
-
-        if (isNetworkAvailable()) {
-            toggleRefresh();
-
-            OkHttpClient client = new OkHttpClient();
-            Request request = new Request.Builder().url(googleLocationDataURL).build();
-
-            //DRY refactor?
-            Call call = client.newCall(request);
-            call.enqueue(new Callback() {
-                @Override
-                public void onFailure(Request request, IOException e) {
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            toggleRefresh();
-                        }
-                    });
-                    alertUserAboutError();
-                }
-
-                @Override
-                public void onResponse(Response response) throws IOException {
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            toggleRefresh();
-                        }
-                    });
-                    try {
-                        String jsonData = response.body().string();
-                        Log.v(TAG, jsonData);
-                        if (response.isSuccessful()) {
-                            mCurrentCity = getCurrentLocation(jsonData);
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    updateDisplay();
-                                }
-                            });
-                        } else {
-                            alertUserAboutError();
-                        }
-                    } catch (IOException e) {
-                        Log.e(TAG, "IO Exception caught: ", e);
-                    } catch (JSONException e) {
-                        Log.e(TAG, "JSON Exception caught: ", e);
-                    }
-                }
-            });
-        } else {
-            Toast.makeText(this, R.string.network_unavailable_msg, Toast.LENGTH_LONG).show();
-        }
-    }
-
     private void getForecast(double latitude, double longitude) {
         String stormyApiKey = getString(R.string.stormyApiKey);
-        String forcastURL = "https://api.forecast.io/forecast/"+ stormyApiKey + "/" + latitude + "," + longitude;
-
+        String forecastURL = "https://api.forecast.io/forecast/"+ stormyApiKey + "/" + latitude + "," + longitude;
 
         if (isNetworkAvailable()) {
             toggleRefresh();
 
-            OkHttpClient client = new OkHttpClient();
-            Request request = new Request.Builder().url(forcastURL).build();
+            OkHttpClient client = new OkHttpClient(
+
+            );
+            Request request = new Request.Builder().url(forecastURL).build();
 
             Call call = client.newCall(request);
             call.enqueue(new Callback() {
@@ -236,24 +171,19 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     private void updateDisplay() {
         mTemperatureLabel.setText(mCurrentWeather.getTemperature() + "");
         mTimeLabel.setText("At " + mCurrentWeather.getFormattedTime() + " it will be");
-        mHumidityValue.setText(mCurrentWeather.getHumidity()+ "");
+        mHumidityValue.setText(mCurrentWeather.getHumidity() + "");
         mPrecipValue.setText(mCurrentWeather.getPrecipChance() + "%");
         mSummaryLabel.setText(mCurrentWeather.getSummary());
+        //mLocationLabel.setText("City, State"); UPCOMING - display current city & state with reverse geocoding
 
         Drawable drawable = ContextCompat.getDrawable(this, mCurrentWeather.getIconID());
         mIconImageView.setImageDrawable(drawable);
-
-        //String city = mCurrentCity.getCity();
-        //String state = mCurrentCity.getState();
-        //mLocationLabel.setText( city + " ," + state);
-        mLocationLabel.setText("Test GC");
     }
 
     private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
         JSONObject forecast = new JSONObject(jsonData);
         JSONObject currently = forecast.getJSONObject("currently");
         String timezone = forecast.getString("timezone");
-        LatLng latLng = new LatLng(mLatitude, mLongitude);
 
         //set current weather
         CurrentWeather currentWeather = new CurrentWeather();
@@ -267,22 +197,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         Log.d(TAG, currentWeather.getFormattedTime());
 
         return currentWeather;
-    }
-
-    private CurrentLocation getCurrentLocation(String jsonData) throws JSONException {
-        JSONObject jLocationData = new JSONObject(jsonData);
-        JSONObject jResults = jLocationData.getJSONObject("results");
-        JSONArray jAddressComponents = jResults.getJSONArray("address_components");
-        JSONObject jCityDetails = jAddressComponents.getJSONObject(2);
-        JSONObject jStateDetails = jAddressComponents.getJSONObject(4);
-
-        CurrentLocation currentLocation = new CurrentLocation();
-        //currentLocation.setCity(jCityDetails.getString("short_name"));
-        //currentLocation.setState(jStateDetails.getString("short_name"));
-        currentLocation.setCity("Test");
-        currentLocation.setState("OR");
-        Log.d(TAG, currentLocation.getCity());
-        return currentLocation;
     }
 
     private boolean isNetworkAvailable() {
@@ -304,12 +218,18 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     @Override
     public void onConnected(Bundle connectionHint) {
         Log.i(TAG, "Location services connected.");
-        Location location = LocationServices.FusedLocationApi.getLastLocation(
+        getLocation();
+    }
+
+    private void getLocation() {
+        Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(
                 mGoogleApiClient);
-        if (location == null) {
-            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
-        } else {
-            handleNewLocation(location);
+        Log.d(TAG, "Last Location from OnConnected: " + lastLocation);
+        if (lastLocation != null) {
+            mLatitude = lastLocation.getLatitude();
+            mLongitude = lastLocation.getLongitude();
+            Log.d(TAG, "Lat: " + String.valueOf(mLatitude));
+            Log.d(TAG, "Long: "+ String.valueOf(mLongitude));
         }
     }
 
@@ -317,8 +237,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         Log.d(TAG, location.toString());
         mLatitude = location.getLatitude();
         mLongitude = location.getLongitude();
-        Log.d(TAG, "Lat: " + mLatitude);
-        Log.d(TAG, "Long: "+ mLongitude);
     }
 
     @Override
